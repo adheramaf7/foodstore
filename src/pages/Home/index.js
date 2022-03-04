6f5c5a5c9b074a65b5bf5f479b64bc21f0bb2a24
Adhe Rama F <adheramaf7@gmail.com>
2022-03-04 09:44:15 +0700

    topbar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { SideNav, LayoutSidebar } from 'upkit';
+import TopBar from '../../components/TopBar';
 import menus from './menus';
 
 export default function Home() {
   const content = (
     <div className="md:flex md:flex-row-reverse w-full mr-5 h-full min-h-screen">
-      <div className="w-full md:w-3/4 pl-5 pb-10">Konten utama di sini</div>
+      <div className="w-full md:w-3/4 pl-5 pb-10">
+        <TopBar />
+      </div>
       <div className="w-full md:w-1/4 h-full shadow-lg border-r border-white bg-gray-100">Keranjang belanja di sini</div>
     </div>
   );
